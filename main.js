3c0ebe4d5d17ba0ff68d4a4cd72ade4b8ee7cfd2
1am1mran <imran.abdul.186@gmail.com>
2018-01-02 18:08:24 +0530

    copy button add

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ function myFunction() {
     var inputBoard = document.getElementById("input-board").value;
     document.getElementById("output-board-content").innerHTML = inputBoard;
 }
+// function copyToClipBoard() {   // to copy the whole output code on one click
+//     var copyText = document.getElementById("output-board");
+//     copyText.select();
+//     document.execCommand("Copy");
+// }
